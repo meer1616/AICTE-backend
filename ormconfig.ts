75bebaf6b73efb8efb118cdc614a82d2032f9f8d
Meer Patel <meerpatel.1616@gmail.com>
2022-06-23 05:34:24 +0530

    change .env

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,6 +5,14 @@ import { Auditorium } from "./src/Entities/Auditorium";
 import { Restaurant } from "./src/Entities/Restaurant";
 import { FoodItems } from "./src/Entities/FoodItem";
 import { Order } from "./src/Entities/Order";
+import dotenv from "dotenv"
+dotenv.config()
+
+console.log(process.env.Host);
+console.log(process.env.User);
+console.log(process.env.Port);
+console.log(process.env.ACCESS_TOKEN_SECRET);
+
 
 export const AppDataSource = new DataSource({
     // type: "postgres",
